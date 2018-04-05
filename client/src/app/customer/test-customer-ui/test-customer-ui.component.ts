2bc383b9a085f2ced6b7595d5d6012fee3721f2d
apatel2 <alok.patel@coxautoinc.com>
2018-04-04 21:57:37 -0400

    fixed invoice styling

--- client/src/app/customer/test-customer-ui/test-customer-ui.component.ts
+++ client/src/app/customer/test-customer-ui/test-customer-ui.component.ts
@@ -8,6 +8,7 @@ import { Customer } from 'app/customer/customer.component';
 import { DataSource } from '@angular/cdk/table';
 import { Http } from '@angular/http';
 import { Data } from '@angular/router';
+import { TransactionDtoList } from '../../sell/sale/sale.component';
 
 @Component({
   selector: 'app-test-customer-ui',
@@ -18,25 +19,28 @@ import { Data } from '@angular/router';
 
 export class TestCustomerUiComponent implements  OnInit{
 
-  customerDto: Customer[];
-  displayedColumns = ['id', 'name'];
-  dataSource: MyDataSource;
+  // customerDto: Customer[];
+  // displayedColumns = ['id', 'name'];
+  // dataSource: MyDataSource;
+  transactionDto: TransactionDtoList;
 
-  constructor(private http: Http) {}
+  constructor(private sellService: SellService) {}
 
-  public getData() {
+  // public getData() {
 
-      return this.http.get('http://localhost:8080/getCustomer')
-      .map(response => response.json())
-      .subscribe((cust)=>{
-        this.customerDto = cust;
-        this.dataSource = new MyDataSource(this.customerDto);
-      });
-  }
+  //     return this.http.get('http://localhost:8080/getCustomer')
+  //     .map(response => response.json())
+  //     .subscribe((cust)=>{
+  //       this.customerDto = cust;
+  //       this.dataSource = new MyDataSource(this.customerDto);
+  //     });
+  // }
 
   ngOnInit() {
+
+    this.getTransactionDetail();
    
-    this.getData();
+    //this.getData();
     // Observable.fromEvent(this.filter.nativeElement, 'keyup')
     //   .debounceTime(150)
     //   .distinctUntilChanged()
@@ -47,31 +51,39 @@ export class TestCustomerUiComponent implements  OnInit{
 
 }
 
+getTransactionDetail(){
+
+  this.sellService.getTransactionById(1000)
+  .subscribe((tran)=>{
+  this.transactionDto = tran;
+  })
 }
-export class MyDataSource extends DataSource < any > {
 
-  _filterChange = new BehaviorSubject('');
-  get filter(): string { return this._filterChange.value; }
-  set filter(filter: string) { this._filterChange.next(filter); }
-
- constructor(private dataBase: Data[]) {
-   super();
- }
- /** Connect function called by the table to retrieve one stream containing the data to render. */
- connect(): Observable < Data[] > {
-   const displayDataChanges = [
-      this._filterChange
-   ];
-
-  return Observable.merge(...displayDataChanges).map(() => {
-     return this.dataBase.slice().filter((item: Data) => {
-       let searchStr = (item.name).toLowerCase();
-       return searchStr.indexOf(this.filter.toLowerCase()) != -1;
-     });
-   });
- }
-
- disconnect() {}
 }
+// export class MyDataSource extends DataSource < any > {
+
+//   _filterChange = new BehaviorSubject('');
+//   get filter(): string { return this._filterChange.value; }
+//   set filter(filter: string) { this._filterChange.next(filter); }
+
+//  constructor(private dataBase: Data[]) {
+//    super();
+//  }
+//  /** Connect function called by the table to retrieve one stream containing the data to render. */
+//  connect(): Observable < Data[] > {
+//    const displayDataChanges = [
+//       this._filterChange
+//    ];
+
+//   return Observable.merge(...displayDataChanges).map(() => {
+//      return this.dataBase.slice().filter((item: Data) => {
+//        let searchStr = (item.name).toLowerCase();
+//        return searchStr.indexOf(this.filter.toLowerCase()) != -1;
+//      });
+//    });
+//  }
+
+//  disconnect() {}
+// }
   
 
