749d024f23628ec485d9f2fbfe84fc1a5c92c6af
alokpatel <alokpatel9824107152@gmail.com>
2018-08-11 11:06:36 -0400

    some changex

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -7,6 +7,6 @@ export const environment = {
   production: false,
   loginExpireWithinMinutes: 30, 
   productUrl: 'http://96.73.255.162:5000',
-  userUrl: 'http://96.73.255.162:5000',
-  reportUrl: 'http://96.73.255.162:5000'
+  userUrl: 'http://96.73.255.162:5000/',
+  reportUrl: 'http://96.73.255.162:5000/'
 };
