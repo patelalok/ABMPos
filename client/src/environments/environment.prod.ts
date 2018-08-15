2677fe72dbb396a190d64080a872f506a99d9f8c
alokpatel <alokpatel9824107152@gmail.com>
2018-08-14 21:14:27 -0400

    some chnage

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
 export const environment = {
   production: true,
   loginExpireWithinMinutes: 30, 
-  productUrl: 'http://localhost:8080/',
-  userUrl: 'http://localhost:8080/',
-  reportUrl: 'http://localhost:8080/'
+
+
+  productUrl: 'http://96.73.255.162:6000',
+  userUrl: 'http://96.73.255.162:6000/',
+  reportUrl: 'http://96.73.255.162:6000/'
+
+
+  // productUrl: 'http://localhost:8080/',
+  // userUrl: 'http://localhost:8080/',
+  // reportUrl: 'http://localhost:8080/'
 
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,7 +6,14 @@
 export const environment = {
   production: false,
   loginExpireWithinMinutes: 30, 
-  productUrl: 'http://localhost:8080',
-  userUrl: 'http://localhost:8080',
-  reportUrl: 'http://localhost:8080'
+
+  productUrl: 'http://96.73.255.162:6000',
+  userUrl: 'http://96.73.255.162:6000/',
+  reportUrl: 'http://96.73.255.162:6000/'
+
+  // productUrl: 'http://localhost:8080',
+  // userUrl: 'http://localhost:8080',
+  // reportUrl: 'http://localhost:8080'
+
+  
 };
