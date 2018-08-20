53c66a7d8b34070f64a23a897341bf056829e6d0
alokpatel <alokpatel9824107152@gmail.com>
2018-08-20 17:47:51 -0400

    added part env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,9 +4,14 @@ export const environment = {
 
   // NEO ACCESSORIES
   
-  productUrl: 'http://96.73.255.162:5001',
-  userUrl: 'http://96.73.255.162:5001',
-  reportUrl: 'http://96.73.255.162:5001'
+  // productUrl: 'http://96.73.255.162:5001',
+  // userUrl: 'http://96.73.255.162:5001',
+  // reportUrl: 'http://96.73.255.162:5001'
+
+        // NEO PARTS
+        productUrl: 'http://96.73.255.162:5002',
+        userUrl: 'http://96.73.255.162:5002',
+        reportUrl: 'http://96.73.255.162:5002'
 
 
   // productUrl: 'http://localhost:8080/',
