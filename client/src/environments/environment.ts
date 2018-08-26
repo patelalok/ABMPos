fe265f1e6ab3b286ff4e0eba7fd8cac1a7c7aa60
alokpatel <alokpatel9824107152@gmail.com>
2018-08-26 12:45:33 -0400

    chnae

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   loginExpireWithinMinutes: 30, 
-  // productUrl: 'http://96.73.255.162:5000',
-  // userUrl: 'http://96.73.255.162:5000',
-  // reportUrl: 'http://96.73.255.162:5000'
+  productUrl: 'http://96.73.255.162:5000',
+  userUrl: 'http://96.73.255.162:5000',
+  reportUrl: 'http://96.73.255.162:5000'
 
-  productUrl: 'http://localhost:5000',
-  userUrl: 'http://localhost:5000',
-  reportUrl: 'http://localhost:5000'
+  // productUrl: 'http://localhost:5000',
+  // userUrl: 'http://localhost:5000',
+  // reportUrl: 'http://localhost:5000'
 };
